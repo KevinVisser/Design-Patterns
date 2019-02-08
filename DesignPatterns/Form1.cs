ef714d975910c7bf517091c26fff8f08a0e76d3a
asscherhofstede <asscherhofstede@hotmail.com>
2019-02-08 16:40:27 +0100

    KevinMilfHunter

---
+++ DesignPatterns/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace DesignPatterns
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Doei");
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+            e.Graphics.DrawLine(pen, 20, 10, 300, 100);
+        }
+    }
+}
