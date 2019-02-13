1cef2c329759fd5af8adaefa734ba0036d7be74d
Kevin Visser <kevin.visser.1995@gmail.com>
2019-02-13 09:23:32 +0100

    redraw panel

--- DesignPatterns/Form1.Designer.cs
+++ DesignPatterns/Form1.Designer.cs
@@ -29,25 +29,62 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.RectangleButton = new System.Windows.Forms.Button();
+            this.EllipseButton = new System.Windows.Forms.Button();
+            this.SelectButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(680, 420);
+            this.panel1.Size = new System.Drawing.Size(510, 341);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
-            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // RectangleButton
+            // 
+            this.RectangleButton.Location = new System.Drawing.Point(524, 10);
+            this.RectangleButton.Name = "RectangleButton";
+            this.RectangleButton.Size = new System.Drawing.Size(75, 23);
+            this.RectangleButton.TabIndex = 1;
+            this.RectangleButton.Text = "Rectangle";
+            this.RectangleButton.UseVisualStyleBackColor = true;
+            this.RectangleButton.Click += new System.EventHandler(this.RectangleButton_Click);
+            // 
+            // EllipseButton
+            // 
+            this.EllipseButton.Location = new System.Drawing.Point(524, 39);
+            this.EllipseButton.Name = "EllipseButton";
+            this.EllipseButton.Size = new System.Drawing.Size(75, 23);
+            this.EllipseButton.TabIndex = 2;
+            this.EllipseButton.Text = "Ellipse";
+            this.EllipseButton.UseVisualStyleBackColor = true;
+            this.EllipseButton.Click += new System.EventHandler(this.EllipseButton_Click);
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.Location = new System.Drawing.Point(524, 68);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(75, 23);
+            this.SelectButton.TabIndex = 3;
+            this.SelectButton.Text = "Select";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(881, 496);
+            this.ClientSize = new System.Drawing.Size(661, 403);
+            this.Controls.Add(this.SelectButton);
+            this.Controls.Add(this.EllipseButton);
+            this.Controls.Add(this.RectangleButton);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -58,6 +95,9 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button RectangleButton;
+        private System.Windows.Forms.Button EllipseButton;
+        private System.Windows.Forms.Button SelectButton;
     }
 }
 
--- DesignPatterns/Form1.cs
+++ DesignPatterns/Form1.cs
@@ -7,33 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing;
 
 
 namespace DesignPatterns
 {
     public partial class Form1 : Form
     {
-        
+        Pen pen = new Pen(Color.Black, 5);
+        Graphics g;
+        Rectangle rect;
+        Size userSize = new Size(50, 50);
+        Point location;
+        bool rectangle = true;
+        bool ellipse = false;
+        bool select = false;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
-        {
-            Console.WriteLine("Doei");
+            g = panel1.CreateGraphics();
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
-            
+            location = e.Location;
+            panel1.Invalidate();
+        }
+
+        private void EllipseButton_Click(object sender, EventArgs e)
+        {
+            ellipse = true;
+            rectangle = false;
+            select = false;
+        }
+
+        private void RectangleButton_Click(object sender, EventArgs e)
+        {
+            rectangle = true;
+            select = false;
+            ellipse = false;
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            select = true;
+            rectangle = false;
+            ellipse = false;
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
-            e.Graphics.DrawLine(pen, 20, 10, 300, 100);
+            location.X -= userSize.Width / 2;
+            location.Y -= userSize.Height / 2;
+            if(rectangle)
+            {
+                rect = new Rectangle(location, userSize);
+                e.Graphics.FillRectangle(new SolidBrush(Color.Black), rect);
+                Console.WriteLine(this.HasChildren);
+            }
+            if (ellipse)
+            {
+                rect = new Rectangle(location, userSize);
+                e.Graphics.FillEllipse(new SolidBrush(Color.Black), rect);
+                Console.WriteLine(this.HasChildren);
+            }
+            if (select)
+            {
+                if (panel1.HasChildren)
+                {
+                    Console.WriteLine(panel1.GetChildAtPoint(location));
+                    Console.WriteLine("Hallo");
+                }
+            }
+            
         }
     }
 }
